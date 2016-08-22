90a6c81be7c6fc82d0762f1be44b8a26089638d9
Milton Carlos Katoo <mckatoo@gmail.com>
2016-08-21 23:41:56 -0300

    FORMATAR FRONT

--- site/admin.php
+++ site/admin.php
@@ -20,6 +20,7 @@
 
     <!-- Custom CSS -->
     <link href="assets/dist/css/sb-admin-2.css" rel="stylesheet">
+    <link href="assets/css/iesi.css" rel="stylesheet">
 
     <!-- Custom Fonts -->
     <link href="assets/bower_components/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
@@ -38,40 +39,8 @@
     <div id="wrapper">
 
         <!-- Navigation -->
-        <nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="index.html">CPA - IESI</a>
-            </div>
-            <!-- /.navbar-header -->
 
-            <ul class="nav navbar-top-links navbar-right">
-                <li class="dropdown">
-                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">
-                        <i class="fa fa-user fa-fw"></i>  <i class="fa fa-caret-down"></i>
-                    </a>
-                    <ul class="dropdown-menu dropdown-user">
-                        <li><a href="#"><i class="fa fa-gear fa-fw"></i> Administrador</a>
-                        </li>
-                        <li class="divider"></li>
-                        <li><a href="login.html"><i class="fa fa-sign-out fa-fw"></i> Sair</a>
-                        </li>
-                    </ul>
-                    <!-- /.dropdown-user -->
-                </li>
-                <!-- /.dropdown -->
-            </ul>
-            <!-- /.navbar-top-links -->
-
-            <div class="navbar-default sidebar" role="navigation">
-            </div>
-            <!-- /.navbar-static-side -->
-        </nav>
+        <?php require (isset($_GET['p'])) ? 'includes/'.$_GET['p'].'.php' : 'includes/navbar.php' ?>
 
         <!-- Page Content -->
         <div id="container theme-showcase">
@@ -102,6 +71,23 @@
 
     <!-- Custom Theme JavaScript -->
     <script src="assets/dist/js/sb-admin-2.js"></script>
+
+    <!-- DataTables JavaScript -->
+    <script src="assets/bower_components/datatables/media/js/jquery.dataTables.min.js"></script>
+    <script src="assets/bower_components/datatables-plugins/integration/bootstrap/3/dataTables.bootstrap.min.js"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="assets/dist/js/sb-admin-2.js"></script>
+    <script type="text/javascript" class="init">
+    $(document).ready(function() {
+        $('#dataTables-example').DataTable({
+            select: true,
+            "language": {
+                "url": "https://cdn.datatables.net/plug-ins/1.10.12/i18n/Portuguese-Brasil.json"
+            }
+        });
+    });
+    </script>
 
 </body>
 
--- site/includes/questao.php
+++ site/includes/questao.php
@@ -1,51 +1,59 @@
 <h1 class="page-header">CPA 2016</h1>
 <form class="ui form" method="post">
 
-<div class="filed">
+<div class="panel form-group">
     <label>Questão</label>
-    <input type="text" name="name" placeholder="Digite um nome">
+    <input class="form-control" type="text" name="name" placeholder="Digite um nome">
     <input type="hidden" name="cadastrar">
+<div class="panel panel-footer panel-footer-m">
+    <button class="btn btn-primary" type="submit"><i class="check green icon"></i>Cadastrar</button>
 </div>
-
-<div style="margin-top: 10px;">
-    <button class="ui blue button" type="submit"><i class="check green icon"></i>Cadastrar</button>
 </div>
 </form>
 
 <div class="ui divider"></div>
-<h2 style="color: green;"><i class="question icon"></i>Lista de questões cadastradas</h2>
 
-<table width="100%" class="ui table">
-    <thead>
-        <tr>
-            <th>Questão</th>
-            <th>Editar</th>
-            <th>Deletar</th>
-        </tr>
-    </thead>
-    <tbody>
-        <?php
-        $questao = new Acme\Models\QuestaoModel;
-        $questaos = $questao->read();
-            foreach($questaos as $questao):
-        ?>
-        <tr>
-            <td><?php echo $questao->questao ?></td>
-            <td>
-                <?php if($questao->questao == 0) ?>
-                    <button type="button" name="btativo" class="btn btn-default btn-circle">
-                        <i class="fa fa-uncheck"></i>
-                    </button>
-                <?php endif ?>
-                <?php if($questao->questao == 1) ?>
-                    <button type="button" name="btativo" class="btn btn-default btn-circle">
-                        <i class="fa fa-check"></i>
-                    </button>
-                <?php endif ?>
-            </td>
-            <td><button class="btn btn-primary">Editar</button></td>
-            <td><button class="btn btn-danger">Deletar</button></td>
-        </tr>
-        <?php endforeach; ?>
-    </tbody>
-</table>
+<div class="panel panel-default">
+    <div class="panel-heading">
+        <h2><i class="fa fa-question-circle fa-fw"></i> Lista de questões cadastradas</h2>
+    </div>
+    <!-- /.panel-heading -->
+    <div class="panel-body">
+        <div class="dataTable_wrapper">
+            <table class="table table-striped table-bordered table-hover" id="dataTables-example">
+                <thead>
+                    <tr>
+                        <th>Questão</th>
+                        <th>Ativo</th>
+                        <th>Editar</th>
+                        <th>Deletar</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    $questao = new Acme\Models\QuestaoModel;
+                    $questaos = $questao->read();
+                        foreach($questaos as $questao):
+                    ?>
+                    <tr class="odd gradeX">
+                        <td><?php echo $questao->questao ?></td>
+                        <td>
+                            <?php if($questao->ativo == 1) {?>
+                                <button type="button" name="btativo" class="btn btn-default btn-circle">
+                                    <i class="fa fa-check"></i>
+                                </button>
+                            <?php }else{ ?>
+                                <button type="button" name="btativo" class="btn btn-default btn-circle">
+                                    <i class="fa fa-uncheck"></i>
+                                </button>
+                            <?php } ?>
+                        </td>
+                        <td><button class="btn btn-primary">Editar</button></td>
+                        <td><button class="btn btn-danger">Deletar</button></td>
+                    </tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
